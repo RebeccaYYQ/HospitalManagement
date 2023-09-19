7303da3eb981ec9d2fc49f73d282970f3636106c
Rebecca Y <micelover12@gmail.com>
2023-09-19 14:09:04 +1000

    Initial setup

---
+++ HospitalManagement/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HospitalManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Utils.MenuHeader("Login");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HospitalManagement/Utils.cs
@@ -0,0 +1,15 @@
+﻿namespace HospitalManagement
+{
+    internal class Utils
+    {
+        public static void MenuHeader(string subHeader)
+        {
+            Console.WriteLine("┌───────────────────────────────────────────┐");
+            Console.WriteLine("│     DOTNET Hospital Management System     │");    
+            Console.WriteLine("├───────────────────────────────────────────┤");
+            Console.WriteLine("│                  {0}                      │", subHeader);
+            Console.WriteLine("└───────────────────────────────────────────┘");
+            Console.WriteLine();
+        }
+    }
+}
